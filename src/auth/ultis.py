8af1f757a96457f56bf5fab4817ef92894548986
HakinRuslan <flowstandoff19@gmail.com>
2024-10-31 23:47:22 +0500

    123

--- src/auth/ultis.py
+++ src/auth/ultis.py
@@ -5,7 +5,7 @@ from auth.models import User
 from fastapi_users.db import SQLAlchemyUserDatabase
 from sqlalchemy.ext.asyncio import AsyncSession
 
-from src.database import get_async_session
+from database import get_async_session
 
 
 async def get_user_db(session: AsyncSession = Depends(get_async_session)):
