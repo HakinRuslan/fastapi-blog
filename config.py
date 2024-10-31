5ad2023419e8cfe5293ddb3bcb7a0d83441e0581
HakinRuslan <flowstandoff19@gmail.com>
2024-11-01 00:02:21 +0500

    123

--- config.py
+++ config.py
@@ -3,8 +3,8 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-user = os.environ.get("user")
-p = os.environ.get("password")
+u = os.environ.get("user")
+psw = os.environ.get("password")
 h = os.environ.get("host")
 p = os.environ.get("port")
 db = os.environ.get("database")
